af45d99097e96a928e171a5c50c4f4cd2649e86d
a67712 <a67712@alunos.ipb.pt>
2025-11-10 17:24:01 +0000

    Using Route

--- App.js
+++ App.js
@@ -1,15 +1,20 @@
-import React from 'react';
-import { View, Text } from 'react-native';
-import { useTranslation } from 'react-i18next';
-import './i18n'; // Importa a configuração de idiomas
+
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './HomeScreen';
+import DetailsScreen from './Details';
+
+const Stack = createStackNavigator();
 
 export default function App() {
-  const { t } = useTranslation(); // Hook para acessar traduções
-
   return (
-    <View>
-      <Text>{t('welcome')}</Text>
-      <Text>{t('message')}</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
